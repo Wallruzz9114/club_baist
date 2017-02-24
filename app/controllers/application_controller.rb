122991b04272e4ba73832b4989eab260e815efbe
jbenpinto <pintojose.benedicto2@gmail.com>
2017-02-23 17:19:49 -0700

    Implemented admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.admin == true
-   #   rails_admin_path
+      rails_admin_path
     elsif current_user.member == true
       reservations_path
     elsif current_user.guest == true
