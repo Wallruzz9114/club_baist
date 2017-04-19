f8c7ac336e2ec1ef1055fe23df43be42a8089a58
Wallruzz9114 <pintojose.benedicto2@gmail.com>
2017-04-19 07:03:02 -0600

    Modified the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :gender, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :gender, :password, :password_confirmation, :current_password) }
   end
 
   def after_sign_in_path_for(resource)
